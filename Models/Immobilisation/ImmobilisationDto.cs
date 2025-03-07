03f5d7d19eab7a811dc3e8b518271f4cfbd0b4a4
RAVO Anderson <andersonravo10@gmail.com>
2025-03-07 10:36:05 +0300

    Ajout des nouvelles fonctionnalités pour le magazine

---
+++ Models/Immobilisation/ImmobilisationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+namespace LimsFrontEnd.Models.Immobilisation
+{
+    public class ImmobilisationDto
+    {
+        [JsonPropertyName("idImmobilisation")]
+        public int IdImmobilisation { get; set; }
+
+        [JsonPropertyName("reference")]
+        public  string? Reference { get; set; }
+
+        [JsonPropertyName("designation")]
+        public  string? Designation { get; set; }
+
+        [JsonPropertyName("idMarque")]
+        public int IdMarque { get; set; }
+
+        [JsonPropertyName("marque")]
+        public MarqueDto? Marque { get; set; }
+    }
+}
---
+++ Models/Outillage/OutillageDto.cs
@@ -0,0 +1,20 @@
+﻿using LimsFrontEnd.Models.Immobilisation;
+using System.Text.Json.Serialization;
+
+namespace LimsFrontEnd.Models.Outillage
+{
+    public class OutillageDto
+    {
+        [JsonPropertyName("idOutillage")]
+        public int IdOutillage { get; set; }
+
+        [JsonPropertyName("designation")]
+        public required string Designation { get; set; }
+
+        [JsonPropertyName("idMarque")]
+        public int? IdMarque { get; set; }
+
+        [JsonPropertyName("marque")]
+        public MarqueDto? Marque { get; set; }
+    }
+}
