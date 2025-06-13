291b08acf3401db89053e3df819afd3c01e81ab0
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2025-06-13 20:57:39 +0300

    Rectification farany

--- Models/TypeTravauxService/TypeTravauxDto.cs
+++ Models/TypeTravauxService/TypeTravauxDto.cs
@@ -7,17 +7,17 @@ public class TypeTravauxDto
     [JsonPropertyName("idTypeTravaux")]
     public int IdTypeTravaux { get; set; }
     [JsonPropertyName("code")]
-    public required string Code { get; set; }
+    public required string Code { get; set; } = "5488";
     [JsonPropertyName("designation")]
-    public required string Designation { get; set; }
+    public required string Designation { get; set; } = "Refroidissement";
     [JsonPropertyName("hasResultat")]
-    public int HasResultat { get; set; }
+    public int HasResultat { get; set; } = 0;
     [JsonPropertyName("idDepartement")]
-    public int IdDepartement { get; set; }
+    public int IdDepartement { get; set; } = 2;
     [JsonPropertyName("departement")]
     public DepartementDto? Departement { get; set; }
     [JsonPropertyName("tarif")]
-    public decimal? Tarif { get; set; }
+    public decimal? Tarif { get; set; } = 35000;
     private DateTime _dateCreation = DateTime.Now;
     [JsonPropertyName("dateCreation")]
     public DateTime DateCreation { 
